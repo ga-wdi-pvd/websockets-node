cff8bda42cf701558ea5374b0dbd53b892b02ec8
estermer <eric.stermer@gmail.com>
2017-01-23 11:08:10 -0500

    added an event listener for io to the connection for the server and console logged some text to test the connection

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// .on is an event listener waiting for a connection from
+// the client at the server port defined by http
+// the callback received a socket as a response object
+io.on('connection', (socket) => {
+  console.log('A connection opened');
+});
+
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
