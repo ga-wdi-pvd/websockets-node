120e5d54372f00aa63053e857428b8a9f2ab91e2
estermer <eric.stermer@gmail.com>
2017-01-23 11:03:00 -0500

    required socket.io after installing it and passing http as an argument

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+// io takes in http as an argument integrating http to the websockets
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
