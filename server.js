fdc6b228aec0162ea233d3a568f5f6dd41e312fe
estermer <eric.stermer@gmail.com>
2017-01-23 10:53:57 -0500

    add http to the server file to be included to the socket.io instance later

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
