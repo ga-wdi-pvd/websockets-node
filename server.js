527bc16f06ce595956139900877cb50d5cf2ac04
estermer <eric.stermer@gmail.com>
2017-01-23 10:04:40 -0500

    added express and opened a server

---
+++ server.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+app.listen(3000, function(){
+  console.log('listening on *:3000');
+});
